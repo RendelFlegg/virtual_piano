e6ea2cca83cc60b30f1cb521cafa36906bca660a
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2021-11-22 15:41:42 +0500

    Completed Stage 4: Added audio

--- Virtual Piano/task/src/assets/js/main.js
+++ Virtual Piano/task/src/assets/js/main.js
@@ -1,5 +1,10 @@
-function message(key){
+function message(key) {
     console.log(`The '${key.toUpperCase()}' is pressed`);
+}
+
+function playAudio(key) {
+    let audio = new Audio(`media/${key.toUpperCase()}.mp3`);
+    audio.play();
 }
 
 const pianoKeys = ["a", "s", "d", "f", "g", "h", "j"];
@@ -7,6 +12,7 @@ const pianoKeys = ["a", "s", "d", "f", "g", "h", "j"];
 document.addEventListener("keydown", (event) => {
     if (pianoKeys.includes(event.key)){
         message(event.key);
+        playAudio(event.key);
     } else{
         console.log("Press piano keypad key");
     }
